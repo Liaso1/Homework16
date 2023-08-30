a6731994db206b101342ab081e19aaf2906b2584
Liaso1 <liasol97@gmail.com>
2023-08-29 22:51:26 -0400

    password

---
+++ Src/User.java
@@ -0,0 +1,20 @@
+public class User {
+    private String login;
+    Acount acount;
+
+    public User(String login, String password) {
+        this.login = login;
+        acount = new Acount(password);
+    }
+    class Acount{
+        private String password;
+
+        public Acount(String password) {
+            this.password = password;
+        }
+        void displayAcount() {
+            System.out.println("Account Login successful! Login: " + login + ", Password: " + password);
+        }
+
+    }
+}
